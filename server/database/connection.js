49acbb47da88a129b68f342f662e712f1c17369e
sailihamza <saili.hamza18@gmail.com>
2022-12-21 21:04:47 +0100

    Update connection.js

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const uri =
-"mongodb+srv://hamzacrud:NyLsOzBwsgbAhGC5@cluster0.sc3spf1.mongodb.net/?retryWrites=true&w=majority"
+"#"
 
 async function connectDB(){
     try{
